2e65061d7c395220c110460f2df8d91f95d2e246
rakshasa <sundell.software@gmail.com>
2016-09-29 02:28:56 +0900

    Added xmlrpc2scgi python script.

--- puppet/modules/builder/files/build-rtorrent.sh
+++ puppet/modules/builder/files/build-rtorrent.sh
@@ -5,14 +5,21 @@ export CXXFLAGS="-Wno-error=strict-aliasing -I/data/shared/pkg/usr/local/include
 export PKG_CONFIG_PATH="/data/shared/pkg/usr/local/lib/pkgconfig"
 export LD_LIBRARY_PATH="/data/shared/pkg/usr/local/lib:$LD_LIBRARY_PATH"
 
-export BUILD_DIR=/data/shared/pkg/
+BUILD_DIR=/data/shared/pkg/
+
+RTORRENT_CONF="--with-xmlrpc-c"
 
 # Build libTorrent.
+#
+# The library is installed to both the packaging and destination in
+# order to satisfy rtorrent's autoconf scripts.
+
 cd /data/local/libtorrent
 ./autogen.sh && ./configure && make || exit 1
+sudo make install || exit 1
 sudo DESTDIR=$BUILD_DIR make install || exit 1
 
 # Build rTorrent.
 cd /data/local/rtorrent
-./autogen.sh && ./configure && make || exit 1
+./autogen.sh && ./configure ${RTORRENT_CONF} && make || exit 1
 sudo DESTDIR=$BUILD_DIR make install || exit 1
---
+++ scripts/xmlrpc2scgi.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+import sys, cStringIO as StringIO
+import xmlrpclib, urllib, urlparse, socket
+
+from urlparse import uses_netloc
+uses_netloc.append('scgi')
+
+def do_scgi_xmlrpc_request(host, methodname, params=()):
+	xmlreq = xmlrpclib.dumps(params, methodname)
+	xmlresp = SCGIRequest(host).send(xmlreq)
+	return xmlresp
+
+def do_scgi_xmlrpc_request_py(host, methodname, params=()):
+	xmlresp = do_scgi_xmlrpc_request(host, methodname, params)
+	return xmlrpclib.loads(xmlresp)[0][0]
+
+class SCGIRequest(object):
+	def __init__(self, url):
+		self.url=url
+		self.resp_headers=[]
+	def __send(self, scgireq):
+		scheme, netloc, path, query, frag = urlparse.urlsplit(self.url)
+		host, port = urllib.splitport(netloc)
+		if netloc:
+			addrinfo = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
+			assert len(addrinfo) == 1, "There's more than one? %r"%addrinfo
+			sock = socket.socket(*addrinfo[0][:3])
+			sock.connect(addrinfo[0][4])
+		else:
+			sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+			sock.connect(path)
+		sock.send(scgireq)
+		recvdata = resp = sock.recv(1024)
+		while recvdata != '':
+			recvdata = sock.recv(1024)
+			resp += recvdata
+		sock.close()
+		return resp
+	def send(self, data):
+		"Send data over scgi to url and get response"
+		scgiresp = self.__send(self.add_required_scgi_headers(data))
+		resp, self.resp_headers = self.get_scgi_resp(scgiresp)
+		return resp
+	@staticmethod
+	def encode_netstring(string):
+		"Encode string as netstring"
+		return '%d:%s,'%(len(string), string)
+	@staticmethod
+	def make_headers(headers):
+		"Make scgi header list"
+		return '\x00'.join(['%s\x00%s'%t for t in headers])+'\x00'
+	@staticmethod
+	def add_required_scgi_headers(data, headers=[]):
+		"Wrap data in an scgi request,\nsee spec at: http://python.ca/scgi/protocol.txt"
+		headers = SCGIRequest.make_headers([('CONTENT_LENGTH', str(len(data))),('SCGI', '1'),] + headers)
+		enc_headers = SCGIRequest.encode_netstring(headers)
+		return enc_headers+data
+	@staticmethod
+	def gen_headers(file):
+		"Get header lines from scgi response"
+		line = file.readline().rstrip()
+		while line.strip():
+			yield line
+			line = file.readline().rstrip()
+	@staticmethod
+	def get_scgi_resp(resp):
+		"Get xmlrpc response from scgi response"
+		fresp = StringIO.StringIO(resp)
+		headers = []
+		for line in SCGIRequest.gen_headers(fresp):
+			headers.append(line.split(': ', 1))
+		xmlresp = fresp.read()
+		return (xmlresp, headers)
+
+class RTorrentXMLRPCClient(object):
+	def __init__(self, url, methodname=''):
+		self.url = url
+		self.methodname = methodname
+	def __call__(self, *args):
+		scheme, netloc, path, query, frag = urlparse.urlsplit(self.url)
+		xmlreq = xmlrpclib.dumps(args, self.methodname)
+		if scheme == 'scgi':
+			xmlresp = SCGIRequest(self.url).send(xmlreq)
+			return xmlrpclib.loads(xmlresp)[0][0]
+		elif scheme == 'http':
+			raise Exception('Unsupported protocol')
+		elif scheme == '':
+			raise Exception('Unsupported protocol')
+		else:
+			raise Exception('Unsupported protocol')
+	def __getattr__(self, attr):
+		methodname = self.methodname and '.'.join([self.methodname,attr]) or attr
+		return RTorrentXMLRPCClient(self.url, methodname)
+
+def convert_params_to_native(params):
+	"Parse xmlrpc-c command line arg syntax"
+	cparams = []
+	for param in params:
+		if len(param) < 2 or param[1] != '/':
+			cparams.append(param)
+			continue
+		if param[0] == 'i':
+			ptype = int
+		elif param[0] == 'b':
+			ptype = bool
+		elif param[0] == 's':
+			ptype = str
+		else:
+			cparams.append(param)
+			continue
+		cparams.append(ptype(param[2:]))
+	return tuple(cparams)
+
+def main(argv):
+	output_python=False
+	if argv[0] == '-p':
+		output_python=True
+		argv.pop(0)
+	host, methodname = argv[:2]
+	respxml = do_scgi_xmlrpc_request(host, methodname, convert_params_to_native(argv[2:]))
+	if not output_python:
+		print respxml
+	else:
+		print xmlrpclib.loads(respxml)[0][0]
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
