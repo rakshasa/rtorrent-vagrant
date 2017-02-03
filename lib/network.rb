3295691cfa083c00f7d1355e32e6f652fa7b62d7
rakshasa <sundell.software@gmail.com>
2017-02-03 19:36:48 +0900

    More stuff.

--- lib/network.rb
+++ lib/network.rb
@@ -7,23 +7,15 @@ def configure_networks(node, config)
   #node.vm.network 'private_network', type: 'static', ip: "fddd::#{config[:ipv6]}/16"
 
   config[:forward] && config[:forward].each { |params| # Replace with named args.
-    if !valid_port(params[:guest]) || !valid_port(params[:host])
-      raise Vagrant::Errors::ConfigInvalid.new
-    end
-
-    node.vm.network 'forwarded_port', guest: params[:guest], host: params[:host]
-
-    node.trigger.after :up do
-      run_remote "echo #{params[:host]} > /data/local/metadata/forward.#{params[:guest]}"
-    end
+    forward_port(node, params)
   }
   
   node.trigger.after :up do
-    if config.has_key?(:enable_ipv4) && config[:enable_ipv4] == false
+    if enable_ipv4?(config)
       run_remote 'disable-inet'
     end
 
-    if config[:ipv6]
+    if enable_ipv6?(config)
       run_remote "change-inet6 fdcc::#{config[:ipv6]}/16"
     else
       run_remote 'disable-inet6'
@@ -31,6 +23,30 @@ def configure_networks(node, config)
   end
 end
 
-def valid_port(port, allow_nil: false)
-  (port || allow_nil) && port > 0 && port < (1 << 16)
+def enable_ipv4?(config)
+  config.has_key?(:enable_ipv4) && config[:enable_ipv4] == false
+end
+
+def enable_ipv6?(config)
+  config[:ipv6]
+end
+
+def valid_port?(port, allow_nil: false)
+  if port
+    port > 0 && port < (1 << 16)
+  else
+    allow_nil
+  end
+end
+
+def forward_port(node, params)
+  if !valid_port?(params[:guest]) || !valid_port?(params[:host])
+    raise Vagrant::Errors::ConfigInvalid.new
+  end
+
+  node.vm.network 'forwarded_port', guest: params[:guest], host: params[:host]
+
+  node.trigger.after :up do
+    run_remote "echo #{params[:host]} > /data/local/metadata/forward.#{params[:guest]}"
+  end
 end
