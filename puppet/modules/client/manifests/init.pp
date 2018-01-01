206d0a52d818a54c265aed59425129f9e3459e59
rakshasa <sundell.software@gmail.com>
2018-01-02 07:22:08 +0900

    Added blocked port ranges for bind port testing.

--- puppet/modules/client/manifests/init.pp
+++ puppet/modules/client/manifests/init.pp
@@ -1,9 +1,13 @@
 class client {
 
   package {
-    []:
+    ['iptables-persistent']:
       ensure => installed,
       require => Exec['update-apt']
+  }
+
+  exec { 'iptables-persistent':
+    command => 'sudo service iptables-persistent restart',
   }
 
   @group { rtorrent: ensure=> present }
@@ -23,42 +27,56 @@ class client {
     group  => 'vagrant'
   }
 
-  file { '/etc/init.d/rtorrent':
-    ensure => file,
+  File {
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
+  }
+
+  file { '/etc/init.d/rtorrent':
+    ensure => file,
     source => 'puppet:///modules/client/rtorrent.init'
   }
 
   file { '/etc/rtorrent.rc':
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
+    mode   => '0644',
     source => 'puppet:///modules/client/rtorrent.rc'
+  }
+
+  file { '/etc/iptables':
+    ensure => directory
+  }
+
+  file { '/etc/iptables/rules.v4':
+    ensure => file,
+    mode   => '0644',
+    source => 'puppet:///modules/client/rules.v4'
+  }
+
+  file { '/etc/iptables/rules.v6':
+    ensure => file,
+    mode   => '0644',
+    source => 'puppet:///modules/client/rules.v6'
   }
 
   file { '/usr/local/bin/start-rtorrent':
     ensure => file,
-    mode   => '0755',
     source => 'puppet:///modules/client/start-rtorrent'
   }
 
   file { '/usr/local/bin/stop-rtorrent':
     ensure => file,
-    mode   => '0755',
     source => 'puppet:///modules/client/stop-rtorrent'
   }
 
   file { '/usr/local/bin/kill-rtorrent':
     ensure => file,
-    mode   => '0755',
     source => 'puppet:///modules/client/kill-rtorrent'
   }
 
   file { '/usr/local/bin/torrent-status':
     ensure => file,
-    mode   => '0755',
     source => 'puppet:///modules/client/torrent-status'
   }
 
